4a98cf0bff7303b50cb9432ed219af6f57dcbbad
manon2546 <manon.m@kkumail.com>
2024-03-17 15:21:45 +0700

    123

--- src/components/Student.vue
+++ src/components/Student.vue
@@ -1,5 +1,50 @@
 <template>
-    <div>
-        <h1>KUTTTTTTTTTTTTTTTTTTTTTTTTTTT</h1>
+  <body style="display: d-flex">
+    <div style="display: flex; justify-content: center; align-items: center">
+      <div
+        class="row featurette"
+        style="display: flex; justify-content: center; align-items: center"
+      >
+        <div class="col-md-7">
+          <h2 class="featurette-heading fw-normal lh-1">
+            สวัสดี
+            <!-- <span class="text-body-secondary">Checkmate.</span> -->
+          </h2>
+          <p class="lead">ยินดีต้อนรับ</p>
+        </div>
+        <div class="col-md-5">
+          <svg
+            class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto"
+            width="1000"
+            height="1000"
+            xmlns="http://www.w3.org/2000/svg"
+            role="img"
+            aria-label="Placeholder: 500x500"
+            preserveAspectRatio="xMidYMid slice"
+            focusable="false"
+            style="
+              background-color: var(--bs-secondary-bg);
+              color: var(--bs-secondary-color);
+            "
+          >
+            <title>Placeholder</title>
+            <rect
+              width="100%"
+              height="100%"
+              fill="var(--bs-secondary-bg)"
+            ></rect>
+            <text x="50%" y="50%" fill="var(--bs-secondary-color)" dy=".3em">
+              500x500
+            </text>
+            <image
+              href="https://www.77kaoded.com/wp-content/uploads/1561580-1024x768.jpg"
+              width="100%"
+              height="100%"
+              preserveAspectRatio="xMidYMid slice"
+            ></image>
+          </svg>
+        </div>
+      </div>
     </div>
+  </body>
 </template>
